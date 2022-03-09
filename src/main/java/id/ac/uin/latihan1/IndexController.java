02688e296755baac59b4857d3f53720b6fe950fa
salmanfazz <salman.fazzz@gmail.com>
2022-03-09 15:50:58 +0700

    Add Parameter Nama + LocalDate

--- src/main/java/id/ac/uin/latihan1/IndexController.java
+++ src/main/java/id/ac/uin/latihan1/IndexController.java
@@ -2,13 +2,16 @@ package id.ac.uin.latihan1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping ("API")
 public class IndexController {
     @GetMapping("hello")
-    public String hello() {
-        return "Selamat Belajar Springboot";
+    public String index(@RequestParam String nama) {
+        return "Selamat Belajar Springboot, " + nama + " Diakses pada : " + LocalDate.now();
     }
 }
