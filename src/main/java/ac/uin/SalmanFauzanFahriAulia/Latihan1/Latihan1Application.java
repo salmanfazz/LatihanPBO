fabd7571560948662cbc3d514259f9691bca920d
salmanfazz <salman.fazzz@gmail.com>
2022-03-09 11:51:33 +0700

    Latihan 1

---
+++ src/main/java/ac/uin/SalmanFauzanFahriAulia/Latihan1/Latihan1Application.java
@@ -0,0 +1,13 @@
+package ac.uin.SalmanFauzanFahriAulia.Latihan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Latihan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Latihan1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ac/uin/SalmanFauzanFahriAulia/Latihan1/Latihan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.uin.SalmanFauzanFahriAulia.Latihan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
